4ade3def6977ef3fcd9fd7fef1b0d456b45a54aa
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-04-06 13:43:44 +0530

    Deleted some garbage files and added char pattern question with soution

--- Patterns/charpattern1.cpp
+++ Patterns/charpattern1.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
-using namespace std;
+/* A
+   BC
+   CDE
+   DEFG
+   EFGHI */
 
+#include <iostream>
+using namespace std;
 
 int main(){
     int n;
+    cout<<"Enter number: ";
     cin>>n;
-    int i = 1;
-    while(i<=n){
-        int j = 1;
+
+    for(int i = 1; i<=n; i++){
         int k = i;
-        while(j<=i){
-            cout<<char('A' + k - 1);
-            j++;
+        for(int j = 1; j<=i; j++){
+            cout<<char('A'+k-1);
             k++;
         }
         cout<<endl;
-        i++;
     }
 }
